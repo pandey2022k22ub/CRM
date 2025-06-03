e4e74c9288344a8acbf870c03fdab71e0ccce85b
pandey2022k22ub <120004915+pandey2022k22ub@users.noreply.github.com>
2025-06-03 17:24:52 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://crm-91or.onrender.com/api';
 
 export const fetchCampaigns = async (userId) => {
   const res = await axios.get(`${API_BASE}/campaigns/${userId}`);
